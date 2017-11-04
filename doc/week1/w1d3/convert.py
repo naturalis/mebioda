be6d817ae60a41d2edae1c2a7b379a6e51e624e2
Rutger Vos <rutgeraldo@gmail.com>
2017-11-04 22:31:13 +0100

    adding converter script

--- doc/week1/w1d3/convert.py
+++ doc/week1/w1d3/convert.py
@@ -1,5 +1,6 @@
 import sys
 from Bio import SeqIO
+from Bio import AlignIO
 from Bio.Align import MultipleSeqAlignment
 
 # usage: python convert.py <infile> <outfile> <format>
@@ -8,8 +9,8 @@ with open(sys.argv[1], "rU") as handle:
     records = {}
     for seq in SeqIO.parse(handle, "fasta"):
         fields  = seq.description.split('|')
-        seq.seq = seq.upper
+        seq.seq = seq.seq.upper()
         records[fields[0]] = seq
     
     aln = MultipleSeqAlignment([ records[key] for key in sorted(records.keys()) ])
-    AlignIO.write(my_alignments, sys.argv[2], sys.argv[3])
+    AlignIO.write(aln, sys.argv[2], sys.argv[3])
