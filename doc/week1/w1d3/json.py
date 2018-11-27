51650e74ee5a7f35fb5855ffbeee3ab7d59acd7b
Rutger Vos <rutgeraldo@gmail.com>
2018-11-27 17:32:23 +0100

    adding json script

---
+++ doc/week1/w1d3/json.py
@@ -0,0 +1,10 @@
+import urllib
+import simplejson as json # sudo pip install simplejson
+url = "http://www.boldsystems.org/index.php/API_Tax/TaxonSearch?taxName=Danaus"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+
+if data['top_matched_names']:
+	for name in data['top_matched_names']:
+		if name['representitive_image']:
+			print name['representitive_image']['image']
