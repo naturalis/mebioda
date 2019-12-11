c184a5c9845a482a9c45870548fd7f16e5e58e35
king-ben <57047255+king-ben@users.noreply.github.com>
2019-12-11 12:08:32 +0100

    updated files for 2019
    Removed last year's script file
    Added and updated a couple of time-saving scripts

--- doc/week3/w3d4/BiSSE_elapids/nicetree.R
+++ doc/week3/w3d4/BiSSE_elapids/nicetree.R
@@ -1,2 +1,2 @@
-plot(tree, cex=0.4, tip.col=rgb(1-chars,0, chars), type="fan")
+plot(tree, cex=0.4, tip.col=rgb(1-v,0, v), type="fan")
 nodelabels(frame="circle", col=rgb(1-asr[2,], 0, asr[2,]), pch=20, cex=1)
